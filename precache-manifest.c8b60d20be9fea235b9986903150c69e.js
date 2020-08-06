c7078b474fa2fd630ccd3c20dace3513582481fc
Rikki Kendall <yrk3@duke.edu>
2020-08-06 10:42:13 -0400

    production build

---
+++ precache-manifest.c8b60d20be9fea235b9986903150c69e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a142d72b484105a0e4a448784505b54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ae5c95c984caba26b30",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3bb9c8aefca3bec0aeb2",
+    "url": "/static/js/2.42531307.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.42531307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae5c95c984caba26b30",
+    "url": "/static/js/main.0b809aef.chunk.js"
+  },
+  {
+    "revision": "352353811d42eee32704",
+    "url": "/static/js/runtime-main.e49ee613.js"
+  }
+]);
